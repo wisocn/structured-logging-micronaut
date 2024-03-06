535c7385e109f4806d1b7d96ae298d29381485bb
Vid Visocnik <vid.visocnik@paymentology.com>
2024-03-06 14:24:36 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A very basic flake";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+
+  outputs = { self, nixpkgs }: {
+
+    devShell.aarch64-darwin = let
+        pkgs = import nixpkgs { system = "aarch64-darwin"; };
+        # choose our preferred jdk package
+        jdk = pkgs.jdk21;
+    in pkgs.mkShell {
+        buildInputs = with pkgs; [
+          jdk
+          # customise gradle
+          (callPackage gradle-packages.gradle_8 {
+            java = jdk;
+          })
+        ];
+    };
+  };
+}
