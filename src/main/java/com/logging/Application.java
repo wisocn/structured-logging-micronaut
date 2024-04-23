1fd3609eb3c1566c83dc853920bfe821eb10e915
Vid Visocnik <vid.visocnik@paymentology.com>
2024-04-23 15:53:54 +0200

    checkpoint

--- src/main/java/com/logging/Application.java
+++ src/main/java/com/logging/Application.java
@@ -1,10 +1,20 @@
 package com.logging;
 
+import ch.qos.logback.classic.LoggerContext;
 import io.micronaut.runtime.Micronaut;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Application {
+
+
+    /**
+     * @return The logback {@link LoggerContext}
+     */
+    private static LoggerContext getLoggerContext() {
+        return (LoggerContext) LoggerFactory.getILoggerFactory();
+    }
 
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
